6605ed2a0450b9c242d06afab02cd46722714732
Tetsuo Miyake <tmky1919mnstwebcamp@gmail.com>
2022-03-02 13:09:54 +0000

    remeshiterro

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '116a40ac05b96b4021a90c3f574e1c555973b6fe03a29b29bc20981c2bfeb75763babbf43e59284726c170972a4b2eb04cfcd898f2464821f826d738ec880269'
